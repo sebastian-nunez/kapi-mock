03711ada2983e54e8e08b7657341322000c4760b
sebastian-nunez <02snunez@gmail.com>
2023-07-10 16:11:45 -0400

    added .env warning

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,14 @@
 import { createClient } from "@supabase/supabase-js"
 
+if (
+  !import.meta.env.VITE_SUPABASE_URL ||
+  !import.meta.env.VITE_SUPABASE_API_KEY
+) {
+  throw Error(
+    "Please include the .env with 'VITE_SUPABASE_URL' and 'VITE_SUPABASE_API_KEY'. Checkout the '.env.sample'"
+  )
+}
+
 const URL = import.meta.env.VITE_SUPABASE_URL
 const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY
 
