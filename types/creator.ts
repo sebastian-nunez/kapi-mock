97ea590be7959330b46b3384367e6f9248ce5640
sebastian-nunez <02snunez@gmail.com>
2023-07-10 01:50:45 -0400

    creator type added

---
+++ types/creator.ts
@@ -0,0 +1,9 @@
+export interface CreatorType {
+  name: string
+  description: string
+  image: string
+  youtube: string | ""
+  twitter: string | ""
+  linkedin: string | ""
+  instagram: string | ""
+}
