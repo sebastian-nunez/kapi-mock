041c53808b18987b3d42bd82ab6441ccbf73bacb
sebastian-nunez <02snunez@gmail.com>
2023-07-10 01:51:24 -0400

    implemented the CreatorForm within the EditorModal. Populates from actual data

--- src/components/EditorModal.tsx
+++ src/components/EditorModal.tsx
@@ -1,25 +1,70 @@
-interface EditorModalProps {
+import { useState } from "react"
+import CreatorForm from "./CreatorForm"
+import { CreatorType } from "../../types/creator"
+
+interface EditorModalProps extends CreatorType {
   modalId: string
-  creatorName: string
 }
 
-const EditorModal: React.FC<EditorModalProps> = ({ modalId, creatorName }) => {
+const EditorModal: React.FC<EditorModalProps> = (props) => {
+  const [name, setName] = useState(props.name)
+  const [description, setDescription] = useState(props.description)
+  const [image, setImage] = useState(props.image)
+  const [youtube, setYoutube] = useState(props.youtube)
+  const [instagram, setInstagram] = useState(props.instagram)
+  const [twitter, setTwitter] = useState(props.twitter)
+  const [linkedin, setLinkedin] = useState(props.linkedin)
+
+  const resetForm = () => {
+    setName("")
+    setDescription("")
+    setImage("")
+    setYoutube("")
+    setInstagram("")
+    setTwitter("")
+    setLinkedin("")
+  }
+
   const handleDelete = () => {
-    console.log("Delete: " + creatorName)
+    console.log("Deleting: " + name)
+
+    // close and reset the form
+    resetForm()
+    document.getElementById(props.modalId)?.click()
   }
 
   const handleSave = () => {
-    console.log("Save info for: " + creatorName)
+    if (name.length <= 0) {
+      alert("Please include the creators name!")
+      return
+    }
+
+    console.log("Saving: ")
+
+    console.table({
+      name,
+      description,
+      image,
+      youtube,
+      instagram,
+      twitter,
+      linkedin,
+    })
+
+    // close and reset the form
+    resetForm()
+    document.getElementById(props.modalId)?.click()
   }
 
   return (
     <>
       {/* Put this part before </body> tag */}
-      <input type="checkbox" id={modalId} className="modal-toggle" />
+      <input type="checkbox" id={props.modalId} className="modal-toggle" />
       <div className="modal">
         <div className="modal-box w-11/12 max-w-4xl">
+          {/* Modal Header */}
           <div className="flex justify-between">
-            <h3 className="text-4xl font-bold">Editor Mode</h3>
+            <h3 className="text-4xl font-bold">Edit Mode</h3>
             <button
               className="btn btn-sm btn-warning rounded-2xl"
               onClick={handleDelete}
@@ -28,23 +73,31 @@ const EditorModal: React.FC<EditorModalProps> = ({ modalId, creatorName }) => {
             </button>
           </div>
 
-          <p className="py-4">
-            {creatorName}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            nisi a reiciendis, similique ea omnis temporibus magnam iusto. Modi
-            dicta totam necessitatibus architecto deleniti numquam placeat
-            adipisci consectetur similique ipsam. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Numquam dolores rerum voluptatem quod
-            id error doloribus aliquid, consequatur quasi dolor exercitationem
-            ipsa quae laudantium dolore blanditiis. Hic perspiciatis eveniet
-            dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Pariatur alias eveniet sapiente voluptatibus perferendis nobis
-            minima in molestiae animi numquam, ipsam totam quod. Hic excepturi
-            quia quis labore velit illo.
-          </p>
+          {/* Modal Body */}
+          <CreatorForm
+            name={name}
+            setName={setName}
+            description={description}
+            setDescription={setDescription}
+            image={image}
+            setImage={setImage}
+            youtube={youtube}
+            setYoutube={setYoutube}
+            instagram={instagram}
+            setInstagram={setInstagram}
+            linkedin={linkedin}
+            setLinkedin={setLinkedin}
+            twitter={twitter}
+            setTwitter={setTwitter}
+          />
 
-          <div className="modal-action ">
-            <label htmlFor={modalId} className="btn">
+          {/* Modal Footer */}
+          <div className="modal-action">
+            <label
+              htmlFor={props.modalId}
+              className="btn"
+              onClick={() => resetForm()}
+            >
               Cancel
             </label>
 
@@ -54,7 +107,11 @@ const EditorModal: React.FC<EditorModalProps> = ({ modalId, creatorName }) => {
           </div>
         </div>
 
-        <label className="modal-backdrop" htmlFor={modalId}>
+        <label
+          className="modal-backdrop"
+          htmlFor={props.modalId}
+          onClick={() => resetForm()}
+        >
           Close
         </label>
       </div>
