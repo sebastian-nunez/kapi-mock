b6a98e8c7953083867cccb22048ae3543233cde7
sebastian-nunez <02snunez@gmail.com>
2023-09-08 09:04:09 -0400

    refactoring into components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,120 +1,15 @@
+import FeaturedInsights from "./FeaturedInsights"
 import Hero from "./Hero"
-import { Link } from "react-router-dom"
+import Services from "./Services"
 
 const Home: React.FC = () => {
   return (
     <div className="">
       <Hero />
 
-      <section className="px-5 lg:px-8">
-        <h1 className="font-bold text-3xl tracking-tight md:text-4xl text-slate-800">
-          Featured Insights
-        </h1>
+      <FeaturedInsights />
 
-        <hr className="mt-2 bg-slate-400" />
-
-        <div className="grid gap-5 mt-5 md:grid-cols-2">
-          <div className="card lg:card-side bg-base-100 shadow-xl">
-            <figure className="max-h-32 sm:max-h-full lg:ml-8 lg:w-2/3">
-              <img
-                src="/src/assets/doc.png"
-                alt="Album"
-                className="lg:h-4/5 object-cover object-top lg:drop-shadow-md lg:rounded-xl"
-              />
-            </figure>
-
-            <div className="card-body">
-              <h2 className="card-title text-2xl md:text-3xl">
-                Free Advisory Report!
-              </h2>
-
-              <p className="text-slate-500 md:text-lg">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi asperiores omnis labore!
-              </p>
-
-              <div className="card-actions justify-center mt-3">
-                <Link to="/file" className="w-full">
-                  <button className="btn btn-block bg-blue-500 text-white rounded-full hover:bg-blue-600">
-                    Complete
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          <div className="card lg:card-side bg-base-100 shadow-xl">
-            <figure className="max-h-32 sm:max-h-full lg:ml-8 lg:w-2/3">
-              <img
-                src="/src/assets/background.png"
-                alt="Album"
-                className="lg:h-4/5 object-cover object-top lg:drop-shadow-md lg:rounded-xl"
-              />
-            </figure>
-
-            <div className="card-body">
-              <h2 className="card-title text-2xl md:text-3xl">Lorem, ipsum.</h2>
-
-              <p className="text-slate-500 md:text-lg">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi asperiores omnis labore!
-              </p>
-
-              <div className="card-actions justify-end">
-                <button className="btn">Read more</button>
-              </div>
-            </div>
-          </div>
-
-          <div className="card lg:card-side bg-base-100 shadow-xl">
-            <figure className="max-h-32 sm:max-h-full lg:ml-8 lg:w-2/3">
-              <img
-                src="/src/assets/brain.png"
-                alt="Album"
-                className="lg:h-4/5 lg:object-cover lg:object-top lg:drop-shadow-md lg:rounded-xl"
-              />
-            </figure>
-
-            <div className="card-body">
-              <h2 className="card-title text-2xl md:text-3xl">
-                Lorem, ipsum dolor.
-              </h2>
-
-              <p className="text-slate-500 md:text-lg">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Soluta, vitae quidem. Veniam voluptate ipsam perferendis.
-              </p>
-
-              <div className="card-actions justify-end">
-                <button className="btn">Read more</button>
-              </div>
-            </div>
-          </div>
-
-          <div className="card lg:card-side bg-base-100 shadow-xl">
-            <figure className="max-h-32 sm:max-h-full lg:ml-8 lg:w-2/3">
-              <img
-                src="/src/assets/arrow.png"
-                alt="Album"
-                className="lg:h-4/5 lg:object-cover lg:object-top lg:drop-shadow-md lg:rounded-xl"
-              />
-            </figure>
-
-            <div className="card-body">
-              <h2 className="card-title text-2xl md:text-3xl">Lorem, ipsum.</h2>
-
-              <p className="text-slate-500 md:text-lg">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-                repellat rerum, delectus sit voluptatum alias!
-              </p>
-
-              <div className="card-actions justify-end">
-                <button className="btn">Read more</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Services />
     </div>
   )
 }
